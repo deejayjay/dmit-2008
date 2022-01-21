1889513962391dd3b0afc8f1124638c640c1baec
Deepak Joy Jose <deejayjay@zoho.com>
2022-01-21 13:53:52 -0500

    Adds code to read data from the firebase real time database

--- src/js/read.js
+++ src/js/read.js
@@ -1,3 +1,24 @@
+import { ref, get } from "firebase/database";
 import { db } from "./libs/firebase/firebaseConfig";
 
-console.log(db);
+// Get the data load in session storage... Update, Delete
+async function pageInit() {
+    const userRef = ref(db, "users/");
+    const userSnapShot = await get(userRef);
+    const userData = userSnapShot.val();
+    console.log(userData);
+    const users = Object.keys(userData).map((key) => {
+        userData[key].id = key;
+        return userData[key];
+    });
+    console.log(users);
+    //   sessionStorage.setItem("store", userData);
+
+    // Build out the user card component
+    // HTML + CSS UI/View userCard
+    // Template
+
+    // Rendering Component + Data ===> UI
+}
+
+pageInit();
